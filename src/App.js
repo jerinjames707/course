40c8972dd6208d7624a26e5b79e8d3abf24f1a05
jerinjames707 <jerinjames707@gmail.com>
2022-05-19 11:30:43 +0530

    course-develop

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addcourse from './components/Addcourse';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Addcourse/>
+      <BrowserRouter>
+     <Routes>
+      <Route path="/" exact element={<Addcourse/>}/>
+  
+    
+      
+     
+     </Routes>
+     </BrowserRouter>
+
     </div>
   );
 }
--- src/components/Addcourse.js
+++ src/components/Addcourse.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const Addcourse = () => {
     var [title,setTitle]=useState("")
@@ -7,13 +8,14 @@ const Addcourse = () => {
     var [ven,setVen]=useState("")
     var [date,setDate]=useState("")
     
-    const [val, setVal] = useState();
+  
     const subData=()=>{
         const data={"title":title,"duration":duration,"dis":dis,"ven":ven,"date":date}
         console.log(data)
     }
   return (
     <div>
+        <Navbar/>
          <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -21,24 +23,24 @@ const Addcourse = () => {
                 
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label  for="" className="form-label">Course Title</label>
-                    <input value={val} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                    <input  onChange={(e)=>{setTitle(e.target.value)}} className="form-control" placeholder="" type="text"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Duration</label>
-                    <input value={val} onChange={(e)=>{setDuration(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                    <input  onChange={(e)=>{setDuration(e.target.value)}} className="form-control" placeholder="" type="text"/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Discription</label>
-                    <input value={val} onChange={(e)=>{setDis(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                    <input  onChange={(e)=>{setDis(e.target.value)}} className="form-control" placeholder="" type="text"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Venue</label>
-                    <input value={val} onChange={(e)=>{setVen(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                    <input  onChange={(e)=>{setVen(e.target.value)}} className="form-control" placeholder="" type="text"/>
                 </div>
                 
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Date</label>
-                    <input value={val} onChange={(e)=>{setDate(e.target.value)}} className="form-control" placeholder="" type="date"/>
+                    <input  onChange={(e)=>{setDate(e.target.value)}} className="form-control" placeholder="" type="date"/>
                 </div>
                 
                 
